faa15ea25d14526e3381c494bc62fc24f1d0b52c
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2025-04-09 20:07:32 -0500

    Add Person class with basic fields and attributes

---
+++ src/main/java/com/lombok/app/App.java
@@ -0,0 +1,10 @@
+package com.lombok.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/lombok/app/Person.java
@@ -0,0 +1,14 @@
+package com.lombok.app;
+
+import java.time.LocalDate;
+
+public class Person {
+
+    private Long id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String age;
+    private String phoneNumber;
+    private LocalDate dateOfBirth;
+}
