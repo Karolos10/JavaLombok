46ac6c8fb2c3af3f28d361c57dc57de7349f7d98
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2025-04-09 21:24:54 -0500

    Finalize project LombokExample with JSON serialization example and Lombok annotations
    - Implementación de la clase Person con anotaciones de Lombok como @Getter, @Setter, @AllArgsConstructor, @NoArgsConstructor y @ToString.
    - Configuración de Maven con dependencias de Lombok y Jackson para serialización a JSON.
    - Ejemplo de serialización de un objeto Person a formato JSON utilizando Jackson.
    - Ajustes en la clase App para pruebas y demostración.

--- src/main/java/com/lombok/app/App.java
+++ src/main/java/com/lombok/app/App.java
@@ -1,10 +1,35 @@
 package com.lombok.app;
 
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDate;
+
 /**
- * Hello world!
- */
+ * Hello world! */
+//@Slf4j
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        /*System.out.println("Hello World!");
+        System.out.println("Hello, my name is Karlos");*/
+
+        /*Person person = new Person(
+                1L,
+                "Karlos",
+                "Perez",
+                "karlos@gmail.com",
+                30,
+                123456,
+                LocalDate.now());*/
+
+        //log.info(person.toString());
+
+        /*Person person2 = Person.builder()
+                        .id(2L)
+                        .name("Sandra")
+                        .build();*/
+
+        System.out.println(person);
+        //System.out.println(person2);
     }
 }
--- src/main/java/com/lombok/app/Person.java
+++ src/main/java/com/lombok/app/Person.java
@@ -1,14 +1,47 @@
 package com.lombok.app;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
 import java.time.LocalDate;
 
+//@Setter(value = AccessLevel.PUBLIC)
+//@Getter(value = AccessLevel.PUBLIC)
+/*@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@ToString(includeFieldNames = false)
+@EqualsAndHashCode*/
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@FieldDefaults(level = AccessLevel.PRIVATE)
+//Builder
+@Value
+@RequiredArgsConstructor
+@ToString(includeFieldNames = false)
 public class Person {
 
-    private Long id;
-    private String name;
-    private String lastName;
-    private String email;
-    private String age;
-    private String phoneNumber;
-    private LocalDate dateOfBirth;
+    Long id;
+    String name;
+    String lastName;
+    String email;
+    Integer age;
+    Integer phoneNumber;
+    LocalDate dateOfBirth;
+
+    /*@Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                ", phoneNumber=" + phoneNumber +
+                ", dateOfBirth=" + dateOfBirth +
+                '}';
+    }*/
 }
